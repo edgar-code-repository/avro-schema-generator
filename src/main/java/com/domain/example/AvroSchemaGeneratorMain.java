04fbab817a5c50ef67a391b062622b8cca5112e6
Edgar <ecofre@gmail.com>
2023-01-23 01:25:42 -0300

    Added code that generates avro schema for class AvroMessageDTO

--- src/main/java/com/domain/example/AvroSchemaGeneratorMain.java
+++ src/main/java/com/domain/example/AvroSchemaGeneratorMain.java
@@ -1,8 +1,27 @@
 package com.domain.example;
 
+import com.domain.example.dto.AvroMessageDTO;
+import com.fasterxml.jackson.core.PrettyPrinter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.avro.Schema;
+import org.apache.avro.reflect.ReflectData;
+
 public class AvroSchemaGeneratorMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        ReflectData reflectData = ReflectData.get();
+
+        Schema schema = reflectData.getSchema(AvroMessageDTO.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(schema.toString());
+
+        PrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+        String strJson = mapper.writer(prettyPrinter).writeValueAsString(json);
+        System.out.println(strJson);
+
 
     }
 
---
+++ src/main/java/com/domain/example/dto/AvroMessageDTO.java
@@ -0,0 +1,34 @@
+package com.domain.example.dto;
+
+public class AvroMessageDTO {
+
+    private Integer code;
+
+    private String message;
+
+    private String description;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
